9c11586ba5a48938776808af72ae137f1903b1cc
frendikawa <frsnaifla@gmail.com>
2024-06-04 10:40:55 +0700

    dashboard,register,login

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('dashboard', function () {
+    return view('dashboard.pages.index');
+});
