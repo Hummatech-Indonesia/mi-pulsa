d56f716749142a09d1633aad7669d49ba57571c4
frendikawa <frsnaifla@gmail.com>
2024-06-11 08:33:59 +0700

    fix aboutseeder

--- database/seeders/AboutSeeder.php
+++ database/seeders/AboutSeeder.php
@@ -17,7 +17,7 @@ class AboutSeeder extends Seeder
         About::create([
             'title' => 'Tentang MiPulsa',
             'description' => 'MiPulsa adalah platform terpercaya untuk top up pulsa dengan harga terbaik dan pelayanan cepat.',
-            'image' => "{{asset('assets/img/about.jpg')}}",
+            'image' => 'https://themewagon.github.io/startup2/img/about.jpg',
             'phone_number' => "+628123123123",
             // Tambahkan data lainnya jika diperlukan
         ]);
