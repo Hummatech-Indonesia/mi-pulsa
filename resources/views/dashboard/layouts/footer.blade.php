c3fbc8a9eccc80273d350e1923b56ee157cd80ac
frizehero <bag4sk4r4@gmail.com>
2024-06-19 15:56:33 +0700

    mod: update script app.js

--- resources/views/dashboard/layouts/footer.blade.php
+++ resources/views/dashboard/layouts/footer.blade.php
@@ -4,7 +4,7 @@
 <script src="{{ asset('dashboard_assets/dist/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
 <!--  core files -->
 <script src="{{ asset('dashboard_assets/dist/js/app.min.js') }}"></script>
-<!-- <script src="{{ asset('dashboard_assets/dist/js/app.init.js') }}"></script> -->
+<script src="{{ asset('dashboard_assets/dist/js/app.init.js') }}"></script>
 <script src="{{ asset('dashboard_assets/dist/js/app-style-switcher.js') }}"></script>
 <script src="{{ asset('dashboard_assets/dist/js/sidebarmenu.js') }}"></script>
 <script src="{{ asset('dashboard_assets/dist/js/custom.js') }}"></script>
@@ -13,7 +13,7 @@
 <!-- <script src="{{ asset('dashboard_assets/dist/libs/apexcharts/dist/apexcharts.min.js') }}"></script> -->
 <!-- <script src="{{ asset('dashboard_assets/dist/js/dashboard.js') }}"></script> -->
 
-{{-- cdn --}}
+<!-- {{-- cdn --}} -->
 <script src="https://cdn.ckeditor.com/4.19.0/standard/ckeditor.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
