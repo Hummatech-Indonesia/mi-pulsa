c7f713195ad2df8d5da40e73ef0716dcf2a88014
frizehero <bag4sk4r4@gmail.com>
2024-06-19 11:47:15 +0700

    mod: update tampilan topup digiflazz

--- resources/views/dashboard/pages/saldo/index.blade.php
+++ resources/views/dashboard/pages/saldo/index.blade.php
@@ -11,7 +11,7 @@
                 <li>BRI : 213501000291307</li>
                 <li>BNI : 1996888992</li>
             </ul>
-            <p class="py-0 my-0">Minimal Deposit Rp. 50.000</p>
+            <p class="py-0 my-0">Minimal Deposit Rp. 200.000</p>
             <p class="py-0 my-0">Nominal yang ditransfer, disesuaikan dengan response yang didapatkan nanti.</p>
             <p class="py-0 my-0">Notes dimasukkan ketika melakukan transfer.</p>
         </div>
@@ -33,7 +33,7 @@
                         <div class="col">
                             <input type="text" name="amount" id="amount" class="form-control balance-input"
                                 placeholder="Ketik Nominal">
-                            <span class="fw-light fs-2">Minimal Rp. 50.000</span>
+                            <span class="fw-light fs-2">Minimal Rp. 200.000</span>
                         </div>
                         <div class="d-flex gap-3 align-items-center mb-3" style="margin-top: 1rem;">
                             <div class="bg-primary rounded-circle p-3"></div>
