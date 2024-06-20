62071a698f8dc37802980bae9759e62d74af7a97
frizehero <bag4sk4r4@gmail.com>
2024-06-20 08:25:54 +0700

    Update DigiFlazzController.php

--- app/Http/Controllers/DigiFlazzController.php
+++ app/Http/Controllers/DigiFlazzController.php
@@ -111,29 +111,51 @@ class DigiFlazzController extends Controller
         $message = $username . $developmentKey . 'deposit';
         $hash = md5($message);
 
-        $postData = [
+        // $postData = [
+        //     "username" => $username,
+        //     "amount" => intval($data['amount']),
+        //     "Bank" => $data['bank'],
+        //     "owner_name" => auth()->user()->name,
+        //     "sign" => $hash
+        // ];
+        $postData = json_encode([
             "username" => $username,
             "amount" => intval($data['amount']),
             "Bank" => $data['bank'],
             "owner_name" => auth()->user()->name,
             "sign" => $hash
-        ];
+        ]);
 
         try {
-            $response = Http::post('https://api.digiflazz.com/v1/deposit', $postData);
-            $data = $response->json()['data'];
+            // $response = Http::post('https://api.digiflazz.com/v1/deposit', $postData);
+            // $data = $response->json()['data'];
 
-            if ($data['rc'] != '00') {
-                return to_route('dashboard.topup.digiflazz')->with('error', $data['message']);
+            $header = array(
+                'Content-Type: application/json',
+            );
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, 'https://api.digiflazz.com/v1/deposit');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+            $result = curl_exec($ch);
+            // dd($result);
+            $data = json_decode($result)->data;
+
+
+            if ($data->rc != '00') {
+                return to_route('dashboard.topup.digiflazz')->with('error', $data->message);
             }
 
             $this->deposit->store([
-                'rc' => $data['rc'],
-                'amount' => $data['amount'],
-                'notes' => $data['notes']
+                'rc' => $data->rc,
+                'amount' => $data->amount,
+                'notes' => $data->notes
             ]);
 
-            return to_route('dashboard.topup.digiflazz')->with('success', 'Total saldo yang harus anda bayarkan adalah Rp. ' . FormatedHelper::rupiahCurrency($data['amount']) . ' Dan masukkan catatan ' . $data['notes'] . ' Ketika anda melakukan transaksi');
+            return to_route('dashboard.topup.digiflazz')->with('success', 'Total saldo yang harus anda bayarkan adalah Rp. ' . FormatedHelper::rupiahCurrency($data->amount) . ' Dan masukkan catatan ' . $data->notes . ' Ketika anda melakukan transaksi');
         } catch (\Throwable $th) {
             dd($th);
             return to_route('dashboard.topup.digiflazz')->with('error', 'Terjadi kesalahan saat melakukan deposit');
