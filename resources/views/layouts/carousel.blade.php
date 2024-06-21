283e64c2c5c8bc7008dc9fdb3b5700e853dbf68d
frendikawa <frsnaifla@gmail.com>
2024-06-21 09:43:50 +0700

    landingpage - uncomplete

--- resources/views/layouts/carousel.blade.php
+++ resources/views/layouts/carousel.blade.php
@@ -1,30 +1,24 @@
 <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img class="w-100" src="{{ asset('assets/img/carousel-1.jpg') }}" alt="Image">
+            <img class="w-100" src="{{ asset('assets/img/carousel-1.jpg') }}" alt="Topup Pulsa">
             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                 <div class="p-3" style="max-width: 900px;">
-                    <h5 class="text-white text-uppercase mb-3 animated slideInDown">Creative & Innovative</h5>
-                    <h1 class="display-1 text-white mb-md-4 animated zoomIn">Creative & Innovative Digital
-                        Solution</h1>
-                    <a href="quote.html"
-                        class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Free Quote</a>
-                    <a href=""
-                        class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Contact Us</a>
+                    <h5 class="text-white text-uppercase mb-3 animated slideInDown">Cepat dan Mudah</h5>
+                    <h1 class="display-1 text-white mb-md-4 animated zoomIn">Isi Pulsa dan Paket Data</h1>
+                    <a href="topup.html" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Isi Sekarang</a>
+                    <a href="contact.html" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Hubungi Kami</a>
                 </div>
             </div>
         </div>
         <div class="carousel-item">
-            <img class="w-100" src="{{ asset('assets/img/carousel-2.jpg') }}" alt="Image">
+            <img class="w-100" src="{{ asset('assets/img/carousel-2.jpg') }}" alt="Paket Data">
             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                 <div class="p-3" style="max-width: 900px;">
-                    <h5 class="text-white text-uppercase mb-3 animated slideInDown">Creative & Innovative</h5>
-                    <h1 class="display-1 text-white mb-md-4 animated zoomIn">Creative & Innovative Digital
-                        Solution</h1>
-                    <a href="quote.html"
-                        class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Free Quote</a>
-                    <a href=""
-                        class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Contact Us</a>
+                    <h5 class="text-white text-uppercase mb-3 animated slideInDown">Akses Internet Tanpa Batas</h5>
+                    <h1 class="display-1 text-white mb-md-4 animated zoomIn">Nikmati Koneksi Terbaik</h1>
+                    <a href="packages.html" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Lihat Paket</a>
+                    <a href="contact.html" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Hubungi Kami</a>
                 </div>
             </div>
         </div>
