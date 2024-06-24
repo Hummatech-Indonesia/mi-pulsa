562f1275012e096794fa1e214632f82ced9bfb41
frendikawa <frsnaifla@gmail.com>
2024-06-24 09:54:00 +0700

    seeder

--- database/seeders/AboutSeeder.php
+++ database/seeders/AboutSeeder.php
@@ -18,7 +18,7 @@ class AboutSeeder extends Seeder
             'title' => 'Tentang MiPulsa',
             'description' => 'MiPulsa adalah platform terpercaya untuk top up pulsa dengan harga terbaik dan pelayanan cepat.',
             'image' => 'https://themewagon.github.io/startup2/img/about.jpg',
-            'phone_number' => "+628123123123",
+            'phone_number' => "+6285176777785",
             // Tambahkan data lainnya jika diperlukan
         ]);
     }
--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -14,9 +14,9 @@ class ContactSeeder extends Seeder
     public function run(): void
     {
         Contact::create([
-            'phone_number'=>'1234567890',
-            'email'=>'info@hummatech.com',
-            'address'=>'Malang'
+            'phone_number' => '+6285176777785',
+            'email' => 'info@hummatech.com',
+            'address' => 'Perum Permata Regency 1, Blk. 10 No.28, Perun Gpa, Ngijo, Kec. Karang Ploso, Kabupaten Malang, Jawa Timur 65152'
         ]);
     }
 }
