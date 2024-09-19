499d15af7b192e7ddd9b2b03dacea60e62d94ad9
frizehero <bag4sk4r4@gmail.com>
2024-09-19 12:34:06 +0700

    mod: update verifikasi email

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Http\Request;
 
 class VerificationController extends Controller
 {
@@ -37,5 +38,23 @@ class VerificationController extends Controller
         $this->middleware('auth');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
+    }
+
+    /**
+     * Show the email verification notice.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\View\View
+     */
+    public function show(Request $request)
+    {
+        // return $request->user()->hasVerifiedEmail()
+        //     ? redirect($this->redirectPath())
+        //     : view('auth.verify');
+        if ($request->user()->hasVerifiedEmail()) {
+            return redirect($this->redirectPath());
+        } else {
+            echo "Anda harus verifikasi email terlebih dahulu";
+        }
     }
 }
